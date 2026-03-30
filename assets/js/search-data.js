d22abe2940052b6d26ed6917c17c0114e0b36e65
uccollab <balloccu.simone@gmail.com>
2026-03-30 16:01:45 +0000

    Deploying to gh-pages from @ uccollab/uccollab.github.io@f4251c19f695d58cfb62dfaa70dd6f56a44bdc41 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Course materials, schedules, and resources for classes taught.",
